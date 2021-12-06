f7d29554426303f4407a9840ff603c97cdc3f207
scholes <zqt_scholes@163.com>
2021-12-06 22:12:31 +0800

    add parseToArr&&setPriority tester

--- test/test.js
+++ test/test.js
@@ -1,25 +1,67 @@
 let c = require("../src/index.js");
+const {
+    parseToArr
+} = require("../lib/stringToArr.js");
+const {
+    setPriority
+} = require("../lib/setPriority.js");
 
 let obj = {
-    a:false,
-    b:true,
-    c:false,
-    d:true,
-    e:true,
-    f:true,
+    a: false,
+    b: true,
+    c: false,
+    d: true,
+    e: true,
+    f: true,
 }
 
 let conditions = {
-    "a||b": true,
-    "(a||b)&& !d": false,
-    "(a &&(b||c)) || !!d": true,
-    "!f": false,
-    "a||d&&c && (e || f && d) && !d": false
+    "a||b": {
+        val: true,
+        pa: ["a", "||", "b"],
+        sp: ["a", "||", "b"]
+    },
+    "(a||b)&& !d": {
+        val: false,
+        pa: [
+            ["a", "||", "b"], "&&", "!", "d"
+        ],
+        sp: [
+            ["a", "||", "b"], "&&", "!", "d"
+        ]
+    },
+    "(a &&(b||c)) || !!d": {
+        val: true,
+        pa: [
+            ["a", "&&", ["b", "||", "c"]], "||", "!!", "d"
+        ],
+        sp: [
+            ["a", "&&", ["b", "||", "c"]], "||", "!!", "d"
+        ]
+    },
+    "!f": {
+        val: false,
+        pa: ["!", "f"],
+        sp: ["!", "f"]
+    },
+    "a||d&&c && (e || f && d) && !d": {
+        val: false,
+        pa: ["a", "||", "d", "&&", "c", "&&", ["e", "||", "f", "&&", "d"], "&&", "!", "d"],
+        sp: ["a", "||", ["d", "&&", "c", "&&", ["e", "||", ["f", "&&", "d"]], "&&", "!", "d"]]
+    }
 }
 
 
-test("test",()=>{
-    for(let i in conditions){
-        expect(c(i,obj)).toBe(conditions[i]);
-    }
-})
+for (let i in conditions) {
+    test("test parseToArr" + i, () => {
+        expect(parseToArr(i)).toEqual(conditions[i].pa);
+    })
+
+    test("test setPriority" + i, () => {
+        expect(setPriority(conditions[i].pa)).toEqual(conditions[i].sp);
+    })
+
+    test("test " + i, () => {
+        expect(c(i, obj)).toBe(conditions[i].val);
+    })
+}
